56b890f43603e9ac975f3dfe19ab96bf8c8648b2
m-viridis <125144648+m-viridis@users.noreply.github.com>
2024-01-10 22:59:20 -0500

    Add files via upload
    Added class object for Analysis

--- DSI_software/Analysis.py
+++ DSI_software/Analysis.py
@@ -1,47 +1,46 @@
-#import libraries
-
 import requests
 import json
-import matplotlib.pyplot as plt
 import numpy as np
-
+from typing import Any, Optional
+import yaml
 from google.colab import userdata #for personal testing
 
-#import yaml user_config; extract user token
+# n.b. this module will not read in system_config because we will not be plotting anything with this package
+# however, there is a system_config.yml file in the package for you to review
+# to see homework related plotting of GitHub data, please see https://github.com/m-viridis/DSI-SQL/blob/main/DSI-HomeworkSummary-2024-01-16.ipynb 
 
-import yaml
-with open('/configs/user_config.yml', 'r') as user_config:
+# import and read user_config with yaml to extract user token as a global variable
+with open('configs\user_config.yml', 'r') as user_config:
   ghtoken = yaml.safe_load(user_config)
 
 usertoken = list(ghtoken.values())[0]
+print(usertoken)
 
-#create empty lists for GitHub data
-names = []
-stars = []
+class Analysis():
 
-def load_data() -> dict:
-  '''Extract data for the most starred repos from GitHub and store data in the above lists'''
-  res = requests.get(url='https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc', headers={'Authorization': 'Bearer' + usertoken})
+  def __init__(self, analysis_config:str):
+    '''stick usertoken to self'''
+    config = []
+    config.append(usertoken)
 
-  res_json = res.json()
-  res_dicts = res_json['items']
-  #return res_json
+    self.config = config
 
-  for item in res_dicts:
-    names.append(item['name'])
-    stars.append(item['stargazers_count'])
-    pass
+  def load_data(self):
+     '''extract data for the most starred repos from GitHub and store data in 'stars' variable'''
 
-load_data()
-print(stars)
+     res = requests.get(url='https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc', headers={'Authorization': 'Bearer' + usertoken})
+     res_json = res.json()
+     res_dicts = res_json['items']
 
-def compute_analysis(x, y, ax=None):
-  '''Use extracted GitHub data to plot a bar chart with a line for the mean for total stars'''
-  plt.figure()
-  plt.barh(names, stars)
-  plt.title("Most starred GitHub repos")
-  plt.axvline(np.mean(y), color='red', label='mean')
-  plt.legend(bbox_to_anchor = (1.0, 1), loc = 'upper right')
-  plt.show()
+     stars = []
 
-compute_analysis(names, stars)
+     for item in res_dicts:
+       stars.append(item['stargazers_count'])
+     return stars
+
+
+  def compute_analysis(self): #creates local variable
+    '''calculate mean for stars'''
+
+    return sum(stars)/len(stars)
+
