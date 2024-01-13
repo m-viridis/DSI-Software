bd823e17041f8edfd35a31073b6a225fd4dc751f
m-viridis <125144648+m-viridis@users.noreply.github.com>
2024-01-13 16:21:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
   version='0.1.0',
   author='M. viridis',
   packages=['DSI_software'],
-  data_files = [('configs', ['configs\user_config.yml'])] 
+  data_files = [('configs\user_config.yml', ['configs'])],
 )
